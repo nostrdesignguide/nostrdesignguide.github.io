6fda88b6ce33165295c77ea15cf0a865a37820dc
Gigi <dergigi@pm.me>
2023-08-22 09:43:35 +0200

    Deploy website - based on 8d51145a9dbd533efeb72ae4dadaca9b8cf48390

---
+++ assets/js/30a24c52.20c199b8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknostr_design=self.webpackChunknostr_design||[]).push([[453],{8605:s=>{s.exports=JSON.parse('{"label":"hello","permalink":"/blog/tags/hello","allTagsPath":"/blog/tags","count":2}')}}]);
--- docs/reference-designs/navigation.md
+++
@@ -1,7 +0,0 @@
----
-sidebar_position: 6
----
-
-# Navigation
-
-Coming soon...
