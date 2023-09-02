2c62101a0da588f5170854d733824aa3f72617cb
Karnage <karnagebitcoin@gmail.com>
2023-09-02 13:11:37 +0900

    update social image and favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/nostr-cover.jpg',
       navbar: {
         title: 'Nostr Design',
         //logo: {
