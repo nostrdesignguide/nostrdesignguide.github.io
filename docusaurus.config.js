1649267a9991a72bdefd096c81fbc674a13aec1a
Karnage <karnagebitcoin@gmail.com>
2023-09-08 22:24:57 +0900

    updated edit url link to our repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/nostrdesignguide/nostrdesignguide.github.io/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/nostrdesignguide/nostrdesignguide.github.io/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
